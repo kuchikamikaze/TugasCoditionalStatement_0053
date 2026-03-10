c4b24a8643a658003aadd4690a2a22e66e0196ab
Naufal Arif <na713634@gmail.com>
2026-03-10 21:44:14 +0800

    input data

--- BMI.cpp
+++ BMI.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+void inputdata(float &berat, float &tinggi){
+    cout<<"--- Selamat datang mahasiswa kelas B ---"<<endl;
+    cout<<"Masukan berat badan (kg): ";
+    cin>>berat;
+    cout<<"Masukan tinggi badan (meter): ";
+    cin>>tinggi;
+}
